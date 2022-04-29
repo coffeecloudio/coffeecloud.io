58171bb0b8aac82afdc5dd5ff89d49d3736a748e
coffeecloudio <90894357+coffeecloudio@users.noreply.github.com>
2022-04-29 10:59:45 +0800

    Add files via upload

---
+++ static/js/16.6ea81f99.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcoffee-cloud"]=this["webpackJsonpcoffee-cloud"]||[]).push([[16],{302:function(e,i,t){},313:function(e,i,t){"use strict";t.r(i);var c=t(21),o=(t(302),t(3));i.default=function(){return Object(o.jsx)("div",{style:{textAlign:"center",margin:c.isMobile?"20px 0 0":"30px 0"},children:Object(o.jsx)("iframe",{src:"https://www.cognitoforms.com/f/Oi0fcWtzCkOz-gWmjqUZkQ/2",frameBorder:"0",width:c.isMobile?"90%":750,height:c.isMobile?500:980})})}}}]);
+//# sourceMappingURL=16.6ea81f99.chunk.js.map
